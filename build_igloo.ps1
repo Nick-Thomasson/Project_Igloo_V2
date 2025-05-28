c1618670bded0906394feb56be3b2f1a34c2fedc
Nick Thomasson <nthomasson22@yahoo.com>
2025-05-28 11:58:36 -0500

    Delete build_igloo.ps1

--- build_igloo.ps1
+++
@@ -1,2 +0,0 @@
-& "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl /std:c++17 /EHsc /I "C:\VS\vcpkg\installed\x64-windows\include" *.cpp /link /LIBPATH:"C:\VS\vcpkg\installed\x64-windows\lib" pqxx.lib libpq.lib wxmsw32u_core.lib wxbase32u.lib comctl32.lib rpcrt4.lib
